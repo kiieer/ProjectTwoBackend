2dcbb1b0fc0fd14c0f2ea4db8be1c4d073800542
kiieer <christliebkiersten@gmail.com>
2022-05-02 11:25:05 -0400

    lindi password recovery

--- src/main/java/com/tks/project/rest/controller/UsersController.java
+++ src/main/java/com/tks/project/rest/controller/UsersController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tks.project.rest.model.Users;
 import com.tks.project.rest.repository.LoginRepository;
 import com.tks.project.rest.repository.UserRepository;
+import com.tks.project.rest.services.UserServiceImp;
 
 
 @RestController
@@ -25,6 +28,29 @@ public class UsersController {
 	
 	@Autowired
 	LoginRepository loginRepository;
+	
+	@Autowired
+    private UserServiceImp userServiceImp;
+
+    @PostMapping("/forgot-password")
+    public String forgotPassword(@RequestParam (defaultValue = "unknown") String email) {
+
+        String response = userServiceImp.forgotPassword(email);
+
+        if (!response.startsWith("Invalid")) {
+            response = "http://localhost:8081/reset-password?token=" + response;
+        }
+        return response;
+    }
+
+    @PutMapping("/reset-password")
+    public String resetPassword(@RequestParam String token,
+            @RequestParam (defaultValue= "unknown") String password) {
+        System.out.println(token);
+        System.out.println(password);
+        return userServiceImp.resetPassword(token, password);
+    }
+
 	
 	@PostMapping("/user")
 	public Users createUser (@RequestBody Users use) {
